b9bb73c7f67b16c9bebe28eb37518ddd88b4d783
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-14 21:37:14 +0700

    cargo build and cargo build --release

---
+++ hello_rust/target/release/deps/hello_rust-bbca7b126082396f.d
@@ -0,0 +1,5 @@
+/home/nadia/nadia/Projects/rust/hello_rust/target/release/deps/hello_rust-bbca7b126082396f: src/main.rs
+
+/home/nadia/nadia/Projects/rust/hello_rust/target/release/deps/hello_rust-bbca7b126082396f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/release/hello_rust.d
@@ -0,0 +1,1 @@
+/home/nadia/nadia/Projects/rust/hello_rust/target/release/hello_rust: /home/nadia/nadia/Projects/rust/hello_rust/src/main.rs
