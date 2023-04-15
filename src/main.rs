ebe1025cd0b9745dbf6b4b204481ee2145e0084f
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-15 09:33:39 +0700

    argument parameter macro println

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,8 @@ fn main() {
   msg_number = 2;
   let msg = "hello!";
   println!("message number {}: {}", msg_number, msg);
+
+  msg_number = 3;
+  let msg = "hey!";
+  println!("message number {1}: {0}", msg_number, msg);
 }
